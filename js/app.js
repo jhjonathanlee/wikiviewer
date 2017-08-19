d6b75f6afab4ecd8b8f0c703b3483bc6341b13bd
Jonathan Lee <jh.jonathanlee@gmail.com>
2017-08-19 15:32:01 -0400

    shows query results in raw form

--- js/app.js
+++ js/app.js
@@ -1,24 +1,26 @@
 (function () {
-
   var app = angular.module("wikiViewer", []);
-
   var baseUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&limit=10&namespace=0&format=json&search=";
-  var queryStr = encodeURIComponent("Karma");
 
-  app.controller("ViewController", ["$http", "$sce", function($http, $sce) {
-    var view = this;
-    view.raw = [];
+  app.controller("ViewController", function() {
+    this.results = {"data":"default"};
+  });
 
-    var url = baseUrl + queryStr;
-    url = $sce.trustAsResourceUrl(url);
-    console.log(url);
-    
-    $http.jsonp(url, { headers: {"Api-User-Agent": "Wikiviewer"}, jsonCallbackParam: 'callback' }).then(function(data){
-      view.raw = data;
-    }, function(err) {
-      console.log("bam!" + err);
-    });
-
+  app.controller("QueryController", ["$http", "$sce", function($http, $sce) {
+    this.query = "";
+    this.search = function(query, results) {
+      console.log("searching: " + encodeURIComponent(query));
+      var url = baseUrl + query;
+      console.log("url: " + url);
+      url = $sce.trustAsResourceUrl(url);
+      
+      $http.jsonp(url, { headers: {"Api-User-Agent": "Wikiviewer"}, jsonCallbackParam: 'callback' }).then(function(data){
+        console.log("orig results: " + results.test)
+        results.data = data;
+        console.log(results);
+      }, function(err) {
+        console.log("bam!" + err);
+      }); 
+    };
   }]);
-
 })();
