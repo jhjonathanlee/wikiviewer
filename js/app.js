5b965b4506110085b6101498938c64953b247cfa
Jonathan Lee <jh.jonathanlee@gmail.com>
2017-08-20 19:34:45 -0400

    cleaned up console logs and added color

--- js/app.js
+++ js/app.js
@@ -9,14 +9,10 @@
   app.controller("QueryController", ["$http", "$sce", function($http, $sce) {
     this.query = "";
     this.search = function(query, results) {
-      console.log("searching: " + encodeURIComponent(query));
       var url = baseUrl + query;
-      console.log("url: " + url);
       url = $sce.trustAsResourceUrl(url);
       
       $http.jsonp(url, { headers: {"Api-User-Agent": "Wikiviewer"}, jsonCallbackParam: 'callback' }).then(function(json){
-        console.log("orig results: " + results)
-        console.log("data: " + JSON.stringify(json.data[0]));
         let data = json.data;
         results.data = [];
         results.query = data[0];
@@ -28,8 +24,6 @@
         for (let i = 0; i < titles.length; i++) {
           results.data.push({"title": titles[i], "desc": descs[i], "link": links[i]});
         }
-
-        console.log(results);
 
       }, function(err) {
         console.log("bam!" + err);
