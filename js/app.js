6cd46051b2ea9dbabd206ed25f4ab9fec68fef4a
Jonathan Lee <jh.jonathanlee@gmail.com>
2017-08-19 21:10:11 -0400

    custom directive for articles list

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
   var baseUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&limit=10&namespace=0&format=json&search=";
 
   app.controller("ViewController", function() {
-    this.results = {"data":"default"};
+    this.results = {"data":[], "query":""};
   });
 
   app.controller("QueryController", ["$http", "$sce", function($http, $sce) {
@@ -14,13 +14,33 @@
       console.log("url: " + url);
       url = $sce.trustAsResourceUrl(url);
       
-      $http.jsonp(url, { headers: {"Api-User-Agent": "Wikiviewer"}, jsonCallbackParam: 'callback' }).then(function(data){
-        console.log("orig results: " + results.test)
-        results.data = data;
+      $http.jsonp(url, { headers: {"Api-User-Agent": "Wikiviewer"}, jsonCallbackParam: 'callback' }).then(function(json){
+        console.log("orig results: " + results)
+        console.log("data: " + JSON.stringify(json.data[0]));
+        let data = json.data;
+        results.data = [];
+        results.query = data[0];
+
+        let titles = data[1];
+        let descs = data[2];
+        let links = data[3];
+
+        for (let i = 0; i < titles.length; i++) {
+          results.data.push({"title": titles[i], "desc": descs[i], "link": links[i]});
+        }
+
         console.log(results);
+
       }, function(err) {
         console.log("bam!" + err);
       }); 
     };
   }]);
+
+  app.directive("articleList", function() {
+    return {
+      restrict: "E",
+      templateUrl: "directives/article-list.html"
+    };
+  });
 })();
