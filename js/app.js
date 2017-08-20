c26096ac42e79e2b2663454264eddf9f50b9c3aa
Jonathan Lee <jh.jonathanlee@gmail.com>
2017-08-20 19:42:32 -0400

    refactored searching functionality out of app.js and into js/controllers/search.js

--- js/app.js
+++ js/app.js
@@ -1,35 +1,9 @@
 (function () {
-  var app = angular.module("wikiViewer", []);
-  var baseUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&limit=20&namespace=0&format=json&search=";
-
+  var app = angular.module("wikiViewer", ["search"]);
+  
   app.controller("ViewController", function() {
     this.results = {"data":[], "query":""};
   });
-
-  app.controller("QueryController", ["$http", "$sce", function($http, $sce) {
-    this.query = "";
-    this.search = function(query, results) {
-      var url = baseUrl + query;
-      url = $sce.trustAsResourceUrl(url);
-      
-      $http.jsonp(url, { headers: {"Api-User-Agent": "Wikiviewer"}, jsonCallbackParam: 'callback' }).then(function(json){
-        let data = json.data;
-        results.data = [];
-        results.query = data[0];
-
-        let titles = data[1];
-        let descs = data[2];
-        let links = data[3];
-
-        for (let i = 0; i < titles.length; i++) {
-          results.data.push({"title": titles[i], "desc": descs[i], "link": links[i]});
-        }
-
-      }, function(err) {
-        console.log("bam!" + err);
-      }); 
-    };
-  }]);
 
   app.directive("articleList", function() {
     return {
