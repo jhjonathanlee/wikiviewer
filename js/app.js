282cf98f64023784fa16608dc0a18f1768049c03
Jonathan Lee <jh.jonathanlee@gmail.com>
2017-08-19 21:33:47 -0400

    restyled how articles are displayed, adeed paragraph text

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 (function () {
   var app = angular.module("wikiViewer", []);
-  var baseUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&limit=10&namespace=0&format=json&search=";
+  var baseUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&limit=20&namespace=0&format=json&search=";
 
   app.controller("ViewController", function() {
     this.results = {"data":[], "query":""};
